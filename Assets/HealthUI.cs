f331c6d4c5c9c49172748730777e532283cd0093
Mango <mholik@edu.cdv.pl>
2024-12-05 12:53:16 +0100

    healthUI 1 player

---
+++ Assets/HealthUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private Image[] hearts; // Массив иконок сердец
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = hearts.Length; // Устанавливаем текущее здоровье равным количеству сердец
+    }
+
+    // Метод для уменьшения здоровья
+    public void DecreaseHealth()
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth--; // Уменьшаем текущее здоровье
+
+            // Скрываем текущее сердце
+            hearts[currentHealth].enabled = false;
+        }
+    }
+}
